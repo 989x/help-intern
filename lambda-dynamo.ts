7906d233730fd9f04ce47af237c06372b68b1fd0
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-08-03 23:48:50 +0700

    header

--- lambda-dynamo.ts
+++ lambda-dynamo.ts
@@ -1,5 +1,5 @@
-// How to make search using "contains" with DynamoDB
-// https://stackoverflow.com/questions/43793888/how-to-make-search-using-contains-with-dynamodb
+🎧 How to make search using "contains" with DynamoDB
+🧭 https://stackoverflow.com/questions/43793888/how-to-make-search-using-contains-with-dynamodb
 
 const AWS = require('aws-sdk');
 const documentClient = new AWS.DynamoDB.DocumentClient();
@@ -20,8 +20,8 @@ documentClient.scan(params, function(err, data) {
     console.log(data);
 });
 
-// scan\query between two timestamps
-// https://stackoverflow.com/questions/35828968/scan-query-between-two-timestamps
+🎧 scan\query between two timestamps
+🧭 // https://stackoverflow.com/questions/35828968/scan-query-between-two-timestamps
 
 function getConversationByDate(req , cb) {
 
@@ -45,13 +45,12 @@ function getConversationByDate(req , cb) {
     });
 }
 
-// How to scan between date range using Lambda and DynamoDB?
-// https://stackoverflow.com/questions/40390386/how-to-scan-between-date-range-using-lambda-and-dynamodb
+🎧 How to scan between date range using Lambda and DynamoDB?
+🧭 https://stackoverflow.com/questions/40390386/how-to-scan-between-date-range-using-lambda-and-dynamodb
 FilterExpression: "start_date BETWEEN :date1 and :date2"
 
-// DynamoDB Filter Expressions (Ultimate Guide w/ Examples)
-// and more... 
-// https://dynobase.dev/dynamodb-filterexpression/
+🎧 DynamoDB Filter Expressions (Ultimate Guide w/ Examples) and more... 
+🧭 https://dynobase.dev/dynamodb-filterexpression/
 
 // 🎃 EXAM - Multiple Filtering with a Filter Expression 
 var params = {
